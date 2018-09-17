9fc132d93ed4c8fcb263a1c1c2ad65fe6f48100d
ezcorn <ezcorn@outlook.com>
2018-09-17 17:39:10 +0800

    add make depend file functions

--- server.go
+++ server.go
@@ -9,8 +9,15 @@ type RouteMap map[string]func(http.ResponseWriter, *http.Request)
 
 func InitServer(routeMap RouteMap, port int, repository string) {
 	initDependTask(repository)
+	http.HandleFunc("/goe/makeDependFile", makeDependFile)
 	for route, action := range routeMap {
-		http.HandleFunc(route, action)
+		http.HandleFunc(route, func(writer http.ResponseWriter, request *http.Request) {
+			action(writer, request)
+		})
 	}
 	http.ListenAndServe(":"+strconv.Itoa(port), nil)
+}
+
+func makeDependFile(writer http.ResponseWriter, request *http.Request) {
+
 }
