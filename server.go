5480c8c2c0e5ef08856e4340f72345c425ca58b4
ezcorn <ezcorn@outlook.com>
2018-09-17 17:54:23 +0800

    set RouteMap to public

--- server.go
+++ server.go
@@ -7,10 +7,12 @@ import (
 
 type RouteMap map[string]func(http.ResponseWriter, *http.Request)
 
-func InitServer(routeMap RouteMap, port int, repository string) {
+var HttpRouteMap RouteMap = make(RouteMap)
+
+func InitServer(port int, repository string) {
 	initDependTask(repository)
 	http.HandleFunc("/goe/makeDependFile", makeDependFile)
-	for route, action := range routeMap {
+	for route, action := range HttpRouteMap {
 		http.HandleFunc(route, func(writer http.ResponseWriter, request *http.Request) {
 			action(writer, request)
 		})
