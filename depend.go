64bf37555fc101a1cc200513c795e04b1420cfd1
ezcorn <ezcorn@outlook.com>
2018-09-17 17:31:54 +0800

    add CallMethod function

--- depend.go
+++ depend.go
@@ -5,17 +5,27 @@ import (
 	"time"
 )
 
+const local = "depend"
+
+type Input interface{}
+
+type Output interface{}
+
 func initDependTask(repository string) {
 	go func() {
 		duration := time.Minute * 5
 		for {
 			// Clone serviceRepo to local
-			os.RemoveAll("depend")
-			gitClone(repository, "depend")
+			os.RemoveAll(local)
+			gitClone(repository, local)
 			// Read service json to memory
 
 			// Sleep some time
 			time.Sleep(duration)
 		}
 	}()
+}
+
+func CallMethod(depend string, action string, input Input) Output {
+	return nil
 }
