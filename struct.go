38874491b23078ccbee4bc683c1ce4582c45f1c3
ezcorn <ezcorn@outlook.com>
2018-09-18 16:45:03 +0800

    add init map code

--- struct.go
+++ struct.go
@@ -4,38 +4,54 @@ import (
 	"net/http"
 )
 
-type NormFunc = func(http.ResponseWriter, *http.Request)
+//
+type Execute = func(http.ResponseWriter, *http.Request)
 
-type NormFuncListen = func(http.ResponseWriter, *http.Request) NormFunc
+//
+type ExecRet = func(http.ResponseWriter, *http.Request) Execute
+
+//
+type Actions map[string]Action
+
+//
+type Listens map[string]Listen
 
 type Config struct {
-	Name    string            `json:"name"`
-	Author  string            `json:"author"`
-	Gateway string            `json:"gateway"`
-	Vendor  string            `json:"vendor"`
-	Actions map[string]Action `json:"actions"`
+	Name    string  `json:"name"`    //
+	Author  string  `json:"author"`  //
+	Gateway string  `json:"gateway"` //
+	Vendor  string  `json:"vendor"`  //
+	Actions Actions `json:"actions"` //
 }
 
 type Action struct {
-	Name    string            `json:"name"`
-	Method  string            `json:"method"`
-	Comment string            `json:"comment"`
-	Listens map[string]Listen `json:"listens"`
-	Execute NormFunc          `json:"-"`
+	Name    string  `json:"name"`    //
+	Method  string  `json:"method"`  //
+	Comment string  `json:"comment"` //
+	Listens Listens `json:"listens"` //
+	Execute Execute `json:"-"`       //
 }
 
 type Listen struct {
-	Name    string            `json:"name"`
-	Comment string            `json:"comment"`
-	Actions map[string]Action `json:"actions"`
-	Execute NormFuncListen    `json:"-"`
+	Name    string  `json:"name"`    //
+	Comment string  `json:"comment"` //
+	Actions Actions `json:"actions"` //
+	Execute ExecRet `json:"-"`       //
 }
 
 func (listen Listen) Include(route string) {
 	// Link listen and action
 	if action, exist := GlobalAction[route]; exist {
+		// Init listen.Actions
+		if listen.Actions == nil {
+			listen.Actions = make(Actions)
+		}
 		// Add action to listen
 		listen.Actions[route] = action
+		// Init action.Listens
+		if action.Listens == nil {
+			action.Listens = make(Listens)
+		}
 		// Add listen to action
 		action.Listens[listen.Name] = listen
 	}
