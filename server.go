a4f0920864b1285eac1c4229c34b94801642f9bc
ezcorn <ezcorn@outlook.com>
2018-09-20 10:30:35 +0800

    fix bugs

--- server.go
+++ server.go
@@ -21,7 +21,7 @@ func InitServer(port int) {
 
 	// TODO: Register to handle
 	for route, action := range actionRegistry {
-		log.Println(jsonEncode(action))
+		log.Println(jsonEncode(*action))
 		http.HandleFunc(route, func(writer http.ResponseWriter, request *http.Request) {
 			for _, listen := range action.Listens {
 				result := listen.Execute(writer, request)
